01e2a6ec8bdf89fb1f9e432633199883eceff0cb
Jason Lee <alihan.lee@gmail.com>
2018-05-25 13:11:44 +0800

    init

--- shipitfile.js
+++ shipitfile.js
@@ -10,17 +10,41 @@ module.exports = shipit => {
       ignores: ['.git', 'node_modules'],
       keepReleases: 2,
       deleteOnRollback: false,
-      shallowClone: true
+      shallowClone: true,
+      npm: {
+        installFlags: ['--production']
+      }
     },
     staging: {
-      servers: ['dc2-user@116.85.9.252']
+      servers: ['dc2-user@116.85.9.252'],
+      branch: 'master'
+    },
+    production: {
+      servers: [{
+        host: '114.116.29.182',
+        user: 'jason'
+      }]
     },
     development: {
-      servers: ['jason@localhost']
+      servers: ['jason@localhost'],
+      branch: 'master'
     },
+    test: {
+      servers: ['jason@localhost'],
+      branch: 'master'
+    }
   })
 
-  shipit.task('pwd', function() {
+  shipit.task('build', function() {
+    // ...
+    shipit.emit('built')
+  })
+
+  shipit.task('start-server', function() {
     return shipit.remote('pwd');
+  })
+
+  shipit.on('built', function() {
+    shipit.start('start-server')
   })
 }
